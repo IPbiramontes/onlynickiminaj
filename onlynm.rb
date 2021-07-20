d1fab78b53c4c69ece684ec8aa5f52cd370b3b04
Isa Biramontes <isabiramontes@gmail.com>
2021-07-20 00:21:55 -0400

    Add files via upload

---
+++ onlynm.rb
@@ -0,0 +1,11 @@
+live_loop :only do
+  sample :elec_triangle, pre_amp: 50, pitch: -70
+  sleep 1
+  sample :elec_triangle, pre_amp: 50, pitch: -69
+  sleep 1
+  sample :elec_triangle, pre_amp: 50, pitch: -68
+  sleep 1
+  sample :elec_triangle, pre_amp: 50, pitch: -69
+  sleep 1
+  sample :elec_triangle, pre_amp: 50, pitch: -70
+end
